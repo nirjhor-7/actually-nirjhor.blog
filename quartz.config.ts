882f9b99353bcd0278decf6e5327bb4b5a968263
Nirjhor <hasan.nirjhor2005@gmail.com>
2026-02-07 20:05:54 +0600

    Quartz sync: Feb 7, 2026, 8:05 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "<i>nirjhor.flow</i>",
+    pageTitle: <i>"nirjhor.flow"</i>,
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
