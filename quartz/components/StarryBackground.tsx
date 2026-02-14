4fb9260244c2cefbdabda7444cf9f56c50e09adc
Hasan Nirjhor <hasan.nirjhor2005@gmail.com>
2026-02-14 20:41:08 +0600

    Create StarryBackground component file

---
+++ quartz/components/StarryBackground.tsx
@@ -0,0 +1,15 @@
+// @ts-ignore
+import starryScript from "./scripts/starrybackground.inline"
+import styles from "./styles/starrybackground.scss"
+import { QuartzComponent, QuartzComponentConstructor } from "./types"
+
+const StarryBackground: QuartzComponent = () => {
+  return (
+    <div id="starry-background" />
+  )
+}
+
+StarryBackground.beforeDOMLoaded = starryScript
+StarryBackground.css = styles
+
+export default (() => StarryBackground) satisfies QuartzComponentConstructor
